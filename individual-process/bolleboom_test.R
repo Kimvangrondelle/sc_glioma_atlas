bc23a13b4472e60d22efca159b3aee4c13b23f79
Kimvangrondelle <112894536+Kimvangrondelle@users.noreply.github.com>
2025-02-25 09:38:56 +0100

    Update bolleboom_test.R

--- individual-process/bolleboom_test.R
+++ individual-process/bolleboom_test.R
@@ -18,7 +18,7 @@ bboom <- RunPCA(bboom, features = VariableFeatures(object = bboom))
 DimPlot(bboom, reduction = "pca") 
 ElbowPlot(bboom, ndims = 45)
 
-#12
+
 d <- 16 # -- optimal principal components
 # clustering
 bboom <- FindNeighbors(bboom, dims = 1:d)
@@ -27,7 +27,7 @@ bboom <- RunUMAP(bboom, dims = 1:d)
 # umap to see how cells cluster together
 DimPlot(bboom, reduction = "umap")
 # save object
-saveRDS(bboom, file = "output/bolleboom/H243.rds")
+# saveRDS(bboom, file = "output/bolleboom/H243.rds")
 # find DE genes per cluster using find all markers
 markers <- FindAllMarkers(bboom, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 top10 <- markers %>%
@@ -38,27 +38,3 @@ top10 <- markers %>%
 print(top10)
 
 
-# 
-# bboom <- Read10X(data.dir = "../../mnt/neuro-genomic-1-ro/single_cell_data/Bolleboom-Gao/H243_GBM/H243raw_feature_bc_matrix")
-# bboom <- CreateSeuratObject(counts = bboom, project = "sc_glioma_atlas", min.cells=3, min.features=200)
-# bboom[["percent.mt"]] <- PercentageFeatureSet(bboom, pattern = "^MT")
-# VlnPlot(bboom, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
-# 
-# bboom <- subset(x=bboom, subset = nFeature_RNA > 200 & nFeature_RNA < 6250 & nCount_RNA > 200 & nCount_RNA < 20000 & percent.mt < 2)
-# bboom <- NormalizeData(bboom)
-# bboom <- FindVariableFeatures(bboom, selection.method = "vst", nfeatures = 2000)
-# bboom <- ScaleData(bboom, features = rownames(bboom))
-# bboom <- RunPCA(bboom, features = VariableFeatures(object = bboom))
-# 
-# DimPlot(bboom, reduction = "pca") 
-# ElbowPlot(bboom, ndims = 45)
-# 
-# #12
-# d <- 16
-# bboom <- FindNeighbors(bboom, dims = 1:d)
-# bboom <- FindClusters(bboom, resolution = 0.8)
-# bboom <- RunUMAP(bboom, dims = 1:d)
-# DimPlot(bboom, reduction = "umap")
-# 
-# 
-# saveRDS(bboom, file = "output/bolleboom/H243raw.rds")
